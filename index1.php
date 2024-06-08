9499c3e8e84c8f2f25fbfa12b56999a1795475ce
madhudas <mmadhukumari57@gmail.com>
2024-06-08 13:36:01 +0530

    remove code

--- index1.php
+++ index1.php
@@ -6,8 +6,5 @@ echo "Okkkk";
 echo "this is also for testign purpous";
 echo "test from vs code";
 echo "all set";
-echo "i am changing in github and try to access in vs code";
 
-echo "okkk";
-echo "try agian";
 ?>
