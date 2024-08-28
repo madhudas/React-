715e776ee755cee29f364f69c550a7287fec67e6
madhudas <mmadhukumari57@gmail.com>
2024-08-28 11:53:07 +0530

    git learning

--- Index.php
+++ Index.php
@@ -11,7 +11,11 @@ echo "i am changing in github and try to access in vs code";
 echo "okkk";
 echo "try agian";
 
-echo "try11"
+echo "try11";
+# hello-world
+/*Hello World repository for Git tutorial
+This is an example repository for the Git tutoial on https://www.w3schools.com
 
+This repository is built step by step in the tutorial.*/
 echo "helllo";
 ?>
